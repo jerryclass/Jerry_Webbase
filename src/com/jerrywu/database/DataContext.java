d525234612c0f192f05b0799caa5f3e07419140b
jerryclass <jerryclass@gmail.com>
2013-07-08 14:35:58 +0800

    新增資料庫連線方式

---
+++ src/com/jerrywu/database/DataContext.java
@@ -0,0 +1,37 @@
+package com.jerrywu.database;
+
+import java.lang.reflect.Field;
+import org.json.JSONObject;
+
+/**
+ * 
+ * 抽象類別-資料
+ *
+ */
+public abstract class DataContext implements IDataContext {
+	/**
+	 * 取得表格名稱
+	 * @return 類別所對應的資料表名稱
+	 */
+	public abstract String getTableName();
+	
+	/**
+	 * 取得PK名稱
+	 * @return 表格所對應的PK欄位名稱
+	 */
+	public abstract String getItemName(); 
+	
+	/**
+	 * 取得FK名稱
+	 * @param type 類別
+	 * @return 欄位
+	 */
+	public abstract <T> Field  getForeignKeyField(Class <T> type);
+	
+	/**
+	 * 將DataContext轉為JSONObject型態
+	 * @return JSON 物件
+	 * @throws Exception
+	 */
+	public abstract JSONObject toJSONObject() throws Exception;
+}
---
+++ src/com/jerrywu/database/IDataContext.java
@@ -0,0 +1,37 @@
+package com.jerrywu.database;
+
+import java.lang.reflect.Field;
+import org.json.JSONObject;
+
+/**
+ * 
+ * 資料庫定義介面
+ *
+ */
+public interface IDataContext {
+		/**
+		 * 取得表格名稱
+		 * @return 類別所對應的資料表名稱
+		 */
+		public String getTableName();
+		
+		/**
+		 * 取得PK名稱
+		 * @return 表格所對應的PK欄位名稱
+		 */
+		public String getItemName(); 
+		
+		/**
+		 * 取得FK名稱
+		 * @param type 類別
+		 * @return 欄位
+		 */
+		public <T> Field  getForeignKeyField(Class <T> type);
+		
+		/**
+		 * 將DataContext轉為JSONObject型態
+		 * @return JSONObject
+		 * @throws Exception
+		 */
+		public JSONObject toJSONObject() throws Exception;
+}
